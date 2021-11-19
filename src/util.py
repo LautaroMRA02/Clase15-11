fa50fad8abec4b96a16b5a7472f2447204f19bb8
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-11-19 20:49:52 -0300

    Extra 2 uploaded

--- src/util.py
+++ src/util.py
@@ -53,4 +53,21 @@ o 4
 c 2
 d 2
 '''
-
+def conteoChar(empresa):
+	empresa = str(empresa).lower()
+	e = empresa.replace(" ", "")
+	lista = e
+	diccionario = {}
+	lista = set(lista)
+	for char in lista:
+		conteo = e.count(char)
+		diccionario[char]=conteo
+	sortedDiccionario = sorted(diccionario.items(), key=lambda x: x[1], reverse=True)
+	sortedDiccionario = dict(sortedDiccionario)
+	num = 0
+	for key, value in sortedDiccionario.items(): 
+		if num < 3:
+			num += 1
+			print(key,value)
+		else:
+			break
